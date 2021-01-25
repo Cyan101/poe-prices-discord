8d999a611767feaf20acbe2999c434409ce7982e
Cyan101 <Crazyandroid101@gmail.com>
2021-01-26 02:06:08 +0800

    added rate limit avoidance and cleaned

--- poe-trade.rb
+++ poe-trade.rb
@@ -7,7 +7,7 @@ POE_TRADE_URL = "https://www.pathofexile.com/api/trade/search/Ritual"
 
 
 def search_item(item, is_map)
-  if is_map == true
+  if is_map
     body = {"query":{"status":{"option":"online"},"type":{"option": item,"discriminator":"warfortheatlas"},"stats":[{"type":"and","filters":[],"disabled":false}],"filters":{"trade_filters":{"disabled":true,"filters":{"price":{"min":3}}}}},"sort":{"price":"asc"}}.to_json
   else
     body = {"query":{"status":{"option":"online"},"type": item,"stats":[{"type":"and","filters":[],"disabled":true}],"filters":{"trade_filters":{"filters":{"price":{"min":3}}}}},"sort":{"price":"asc"}}.to_json
@@ -18,10 +18,8 @@ end
 
 def check_prices(ids)
   url = "https://www.pathofexile.com/api/trade/fetch/" + ids['result'].take(10).join(',')
-  puts url
   raw_data = RestClient.get url, {params: {query: ids['id']}}
   data = JSON.parse(raw_data)
-  #puts data['result']
   prices = []
   data['result'].each do |x|
     prices << x['listing']['price']['amount']
@@ -35,4 +33,5 @@ POE_ITEMS.each do |item|
   ids = search_item(item, is_map)
   prices = check_prices(ids)
   PoE_Prices[item] = prices.drop(2)  #Ignoring undercutters
+  sleep 10 # Ratelimit avoidance, might need to increase
 end
