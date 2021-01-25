571d8a1229e85651cd04d70f08bb794d20ca6865
Cyan101 <Crazyandroid101@gmail.com>
2021-01-26 04:26:16 +0800

    re-organized search and how embed is created and updated

--- main.rb
+++ main.rb
@@ -11,44 +11,38 @@ bot.message(with_text: 'Ping!') do |event|
   event.respond 'Pong!'
 end
 
-bot.command(:test, help_available: false) do |event|
-  event.channel.send_embed do |e|
-    e.author = { name: event.bot.profile.name, url: 'http://github.com/cyan101/poe-trade-discord', icon_url: event.bot.profile.avatar_url }
-    e.color = '3498db'
-    e.thumbnail = { url: event.bot.profile.avatar_url }
-    e.title = 'PoE Map/Fragment Pricing'
-    e.description = 'Information on guardian map and fragment pricing updated every 10min'
-    e.add_field name: "1", value: "2", inline: true
-    e.add_field name: "​​", value: "​", inline: true
-    e.add_field name: "1", value: "2", inline: true
-  end
-end
-
 bot.command(:start, help_available: false) do |event|
-  puts event.user.id
-  puts CONFIG.owner
+  puts event.channel.send_message('<:cyanisamazing:540549775531966465>')
   break unless event.user.id == CONFIG.owner
   running = true
 
-  embed_test = event.channel.send_embed do |e|
-    e.author = { name: event.bot.profile.name, url: 'http://github.com/cyan101/poe-trade-discord', icon_url: event.bot.profile.avatar_url }
-    e.color = '3498db'
-    e.thumbnail = { url: event.bot.profile.avatar_url }
-    e.title = 'PoE Map/Fragment Pricing'
-    e.description = 'Information on guardian map and fragment pricing updated every 10min'
-    #
-    Poe_data.each_with_index do |(x, y), i|
-      puts i
-      puts i % 2 != 0 || i == 0
-      e.add_field name: x, value: y, inline: i % 2 != 0 || i == 0
-    end
+  run_pc()
+
+  poe_embed = event.send_message(nil, nil, poe_embed_create(event))
+
+  while running == true
+    sleep 500
+    run_pc()
+    poe_embed.edit(nil, poe_embed_create())
   end
 
-#  while running == true
-#    sleep 5
-#    embed_post.edit "PoE Data: Test " + rand(1..10).to_s
-#  end
 end
 
+def poe_embed_create(x)
+  embed = Discordrb::Webhooks::Embed.new
+  embed.tap do |e|
+    e.author = { name: "PoE Prices", url: 'http://github.com/cyan101/poe-trade-discord', icon_url: x.bot.profile.avatar_url }
+    e.color = '3498db'
+    e.thumbnail = { url: x.bot.profile.avatar_url }
+    e.title = 'PoE Map/Fragment Pricing'
+    e.description = 'Information on guardian map and fragment pricing updated every 10min'
+    e.timestamp = Time.now.utc
+    #
+    PoE_Prices.each_with_index do |(x, y), i|
+      e.add_field name: x, value: y[0].to_s + 'c', inline: true
+      e.add_field(name: "​​", value: "​", inline: true) if i.odd?
+    end
+  end
+end
 
 bot.run
--- poe-trade.rb
+++ poe-trade.rb
@@ -1,15 +1,41 @@
 require 'json';
 require 'rest-client'
 
-POE_ITEMS = ["Lair of the Hydra Map", "Fragment of the Hydra"]
+POE_ITEMS = {"Lair of the Hydra Map" => :shaper_map, "Fragment of the Hydra" => :fragment, "Pit of the Chimera Map" => :shaper_map, "Fragment of the Chimera" => :fragment, "Maze of the Minotaur Map" => :shaper_map, "Fragment of the Minotaur" => :fragment,
+  "Forge of the Phoenix Map" => :shaper_map, "Fragment of the Phoenix" => :fragment, "Elder1" => :elder_map,  "Fragment of Enslavement" => :fragment, "Elder2" => :elder_map, "Fragment of Eradication" => :fragment, "Elder3" => :elder_map,
+  "Fragment of Constriction" => :fragment, "Elder4" => :elder_map, "Fragment of Purification" => :fragment}
+ELDER_NAME_LOOKUP = ["The Enslaver", "The Eradicator", "The Contrictor", "The Purifier"]
 PoE_Prices = {}
 POE_TRADE_URL = "https://www.pathofexile.com/api/trade/search/Ritual"
 
+def run_pc
+  POE_ITEMS.each do |item, type|
+    ids = search_item(item, type)
+    prices = check_prices(ids)
+    x = item[-1].to_i - 1 if type == :elder_map
+    item = ELDER_NAME_LOOKUP[x] if type == :elder_map
+    PoE_Prices[item] = prices.drop(2) #Ignoring undercutters
+    sleep 10 # Ratelimit avoidance, might need to increase
+  end
+end
 
 def search_item(item, is_map)
   if is_map
     body = {"query":{"status":{"option":"online"},"type":{"option": item,"discriminator":"warfortheatlas"},"stats":[{"type":"and","filters":[],"disabled":false}],"filters":{"trade_filters":{"disabled":true,"filters":{"price":{"min":3}}}}},"sort":{"price":"asc"}}.to_json
   else
+    body = {"query":{"status":{"option":"online"},"type": item,"stats":[{"type":"and","filters":[],"disabled":true}],"filters":{"trade_filters":{"filters":{"price":{"min":3}}}}},"sort":{"price":"asc"}}.to_json
+  end
+  data = RestClient.post POE_TRADE_URL, body, {content_type: :json, accept: :json}
+  return JSON.parse(data)
+end
+
+def search_item(item, type)
+  case type
+  when :shaper_map
+    body = {"query":{"status":{"option":"online"},"type":{"option": item,"discriminator":"warfortheatlas"},"stats":[{"type":"and","filters":[],"disabled":false}],"filters":{"trade_filters":{"disabled":true,"filters":{"price":{"min":3}}}}},"sort":{"price":"asc"}}.to_json
+  when :elder_map
+    body = {"query":{"status":{"option":"online"},"stats":[{"type":"and","filters":[{"id":"implicit.stat_3624393862","disabled":false,"value":{"option":item[-1]}}],"disabled":false}],"filters":{"trade_filters":{"filters":{"price":{"min":3}}}}},"sort":{"price":"asc"}}.to_json
+  when :fragment
     body = {"query":{"status":{"option":"online"},"type": item,"stats":[{"type":"and","filters":[],"disabled":true}],"filters":{"trade_filters":{"filters":{"price":{"min":3}}}}},"sort":{"price":"asc"}}.to_json
   end
   data = RestClient.post POE_TRADE_URL, body, {content_type: :json, accept: :json}
@@ -25,13 +51,4 @@ def check_prices(ids)
     prices << x['listing']['price']['amount']
   end
   return prices
-end
-
-POE_ITEMS.each do |item|
-  is_map = false
-  is_map = true if item.include? "Map"
-  ids = search_item(item, is_map)
-  prices = check_prices(ids)
-  PoE_Prices[item] = prices.drop(2)  #Ignoring undercutters
-  sleep 10 # Ratelimit avoidance, might need to increase
 end
