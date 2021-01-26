3251a9cddfa0bf3c50a76b99688491c671613b88
Cyan101 <Crazyandroid101@gmail.com>
2021-01-26 13:25:50 +0800

    Added optional emoji

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'yaml'
 require_relative 'poe-trade.rb'
 
 CONFIG = OpenStruct.new YAML.load_file 'settings.yaml'
+$poe_prices_running = false
 
 bot = Discordrb::Commands::CommandBot.new  token: CONFIG.token, prefix: CONFIG.prefix
 puts bot.invite_url
@@ -19,31 +20,32 @@ end
 
 bot.command(:start, help_available: false) do |event|
   break unless event.user.id == CONFIG.owner
-  running = true
+  $poe_prices_running = true
 
   run_pc()
 
   poe_embed = event.send_message(nil, nil, poe_embed_create(event))
 
-  while running == true
-    sleep 10
+  while $poe_prices_running == true
+    sleep 200
     run_pc()
     poe_embed.edit(nil, poe_embed_create(event))
   end
 
 end
 
-def poe_embed_create(x)
+def poe_embed_create(event)
   embed = Discordrb::Webhooks::Embed.new
   embed.tap do |e|
-    e.author = { name: "PoE Prices", url: 'http://github.com/cyan101/poe-trade-discord', icon_url: x.bot.profile.avatar_url }
+    e.author = { name: "PoE Prices", url: 'http://github.com/cyan101/poe-trade-discord', icon_url: event.bot.profile.avatar_url }
     e.color = '3498db'
-    e.thumbnail = { url: x.bot.profile.avatar_url }
+    e.thumbnail = { url: event.bot.profile.avatar_url }
     e.title = 'PoE Map/Fragment Pricing'
     e.description = 'Shaper/Elder Guardian Maps & Fragments - Updated every 10min'
     e.timestamp = Time.now.utc
     #
     PoE_Prices.each_with_index do |(x, y), i|
+      x = event.server.emoji.values.find { |z| z.name == x.gsub(' ','') }.mention  + ' ' + x if CONFIG.emoji == true
       e.add_field name: x, value: y[0].to_s + 'c', inline: true
       e.add_field(name: "​​", value: "​", inline: true) if i.odd?
     end
